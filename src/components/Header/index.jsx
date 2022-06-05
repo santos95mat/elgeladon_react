825b2ebd5072d0be1f9f9fd88959fa90f4792bf1
matheus <santos95.mat@gmail.com>
2022-06-05 11:26:00 -0300

    favorite header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,14 @@
 import logo from "../../assets/images/logo.png";
 import "./style.css";
+import star from "../../assets/images/star1.png";
 
 const Header = (props) => {
   return (
     <header>
       <img alt="Logo" src={logo} />
+      <a href="##">
+        <img width="32px" src={star} alt="star" />
+      </a>
     </header>
   );
 };
