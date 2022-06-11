378229d8add568f280a16c1324b37a1c30b54aae
matheus <santos95.mat@gmail.com>
2022-06-10 22:32:28 -0300

    loading

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,6 +2,7 @@ import "./style.css";
 import { useState } from "react";
 import star from "../../assets/images/star.png";
 import star1 from "../../assets/images/star1.png";
+import ReactLoading from "react-loading";
 
 const Card = ({ characters, filter }) => {
   const json = JSON.stringify(characters);
@@ -18,24 +19,35 @@ const Card = ({ characters, filter }) => {
 
   return (
     <>
-      {characters
-        .filter((e) => {
-          return e.name.toLowerCase().includes(filter.toLowerCase());
-        })
-        .map((props) => {
-          return (
-            <div key={props.id} className="card">
-              <h3>{props.name}</h3>
-              <img className="img_p" alt="presonagem" src={props.image} />
-              <img
-                onClick={() => getFavorite(props._id)}
-                className="img_s"
-                alt="star"
-                src={showFavorite(props._id)}
-              />
-            </div>
-          );
-        })}
+      {characters.lengh === 0 ? (
+        <ReactLoading
+          type="spinningBubbles"
+          color="lightBlue"
+          height={667}
+          width={375}
+        />
+      ) : (
+        <>
+          {characters
+            .filter((e) => {
+              return e.name.toLowerCase().includes(filter.toLowerCase());
+            })
+            .map((props) => {
+              return (
+                <div key={props.id} className="card">
+                  <h3>{props.name}</h3>
+                  <img className="img_p" alt="presonagem" src={props.image} />
+                  <img
+                    onClick={() => getFavorite(props._id)}
+                    className="img_s"
+                    alt="star"
+                    src={showFavorite(props._id)}
+                  />
+                </div>
+              );
+            })}
+        </>
+      )}
     </>
   );
 };
